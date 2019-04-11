c2b30fbe31ea2cbb4368bd88d995e3dd1f7c3022
Aviv Goll <6596595+avivg@users.noreply.github.com>
2019-04-11 18:25:34 +0300

    Update validate_example.sh to pass

--- validate_example.sh
+++ validate_example.sh
@@ -4,5 +4,5 @@ make -C example || { echo "Make example failed"; exit 3; }
 example/example > out.txt || { echo "Running example failed"; exit 3; }
 fgrep 'x: 100, y: 200' out.txt || { echo "FAIL"; exit 3; }
 fgrep 'x: 300, y: 400' out.txt || { echo "FAIL"; exit 3; }
-(( `cat out.txt | wc -l` == 3 )) || { echo "FAIL"; exit 3; }
+(( `cat out.txt | wc -l` == 2 )) || { echo "FAIL"; exit 3; }
 rm out.txt
