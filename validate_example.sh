efd3260947b45f1cbed488282566107bed5295c4
Aviv Goll <6596595+avivg@users.noreply.github.com>
2019-04-11 18:11:40 +0300

    Update validate example script

--- validate_example.sh
+++ validate_example.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-make -C example
-example/example > out.txt
+make -C example || { echo "Make example failed"; exit 3; }
+example/example > out.txt || { echo "Running example failed"; exit 3; }
 fgrep 'x: 100, y: 200' out.txt || { echo "FAIL"; exit 3; }
 fgrep 'x: 300, y: 400' out.txt || { echo "FAIL"; exit 3; }
 (( `cat out.txt | wc -l` == 3 )) || { echo "FAIL"; exit 3; }
