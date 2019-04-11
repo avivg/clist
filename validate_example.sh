8ec661b8858ea8882b1ffad0fb0af38d52d0ce1a
Aviv Goll <6596595+avivg@users.noreply.github.com>
2019-04-11 18:02:11 +0300

    Validate example application

---
+++ validate_example.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+make -C example
+example/example > out.txt
+fgrep 'x: 100, y: 200' out.txt || { echo "FAIL"; exit 3; }
+fgrep 'x: 300, y: 400' out.txt || { echo "FAIL"; exit 3; }
+(( `cat out.txt | wc -l` == 2 )) || { echo "FAIL"; exit 3; }
+rm out.txt
